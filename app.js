13e54ebf9668b2deb07f4d12013efa3de4f63a51
ryoochi1996 <79794032+ryoochi1996@users.noreply.github.com>
2021-07-27 17:54:28 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+$(function () {
+  $(".fade-img img:not(:first-child)").hide();
+  setInterval(function () {
+    $(".fade-img img:first-child")
+      .fadeOut("slow")
+      .next("img")
+      .fadeIn("slow")
+      .end()
+      .appendTo(".fade-img");
+  }, 3500);
+});
+
+$(function () {
+  $(window).scroll(function () {
+    $(".effect-fade").each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight) {
+        $(this).addClass("effect-scroll");
+      }
+    });
+  });
+});
+
+
+$(function () {
+  $('.box').click(function(e) {
+    e.preventDefault();
+    fade = $(this).parents().next();
+    console.log(fade);
+    fade.fadeIn();
+});
+
+$('.work_modal').click(function() {
+    $(this).fadeOut();
+});
+
+});
